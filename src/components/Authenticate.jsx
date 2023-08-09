8f2868c37af56b51f14c32c425d11c779f8b20f4
Sofie Christensen <sofiec510@gmail.com>
2023-08-09 17:33:16 -0400

    authentication WIP

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,9 +1,31 @@
-export default function Authenticate() {
-  //   const { token, setToken } = props;
+import { useState } from "react";
+
+export default function Authenticate({ token }) {
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [error, setError] = useState(null);
+
+  async function handleClick() {
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/authenticate",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const result = await response.json();
+    } catch (error) {
+      setError(error.message);
+    }
+  }
   return (
     <>
       <h2>Authenticate!</h2>
-      {/* <button onClick={handleClick}>Authenticate Token</button> */}
+      {error && <p>{error}</p>}
+      <button onClick={handleClick}>Authenticate Token</button>
     </>
   );
 }
