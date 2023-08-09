ecbbd3a16f56c0fb732f4e1f4f709a526ff68afe
Sofie Christensen <sofiec510@gmail.com>
2023-08-09 15:22:59 -0400

    try/catch with fetch and post method

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,29 +1,44 @@
 import { useState } from "react";
 
 export default function SignUpForm() {
-  const [userName, setUserName] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
   async function handleSubmit(event) {
     event.preventDefault();
-    console.log("hi");
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/signup",
+        {
+          method: "POST",
+          body: JSON.stringify({ username, password }),
+        }
+      );
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      setError(error.message);
+    }
   }
   return (
     <>
       <h2>Sign Up!</h2>
+      {error && <p>{error}</p>}
       <form onSubmit={handleSubmit}>
         <label>
-          Username:
+          Username: {""}
           <input
-            value={userName}
-            onChange={(event) => setUserName(event.target.value)}
+            // type="text"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
           />
         </label>
         <br />
         <label>
-          Password:
+          Password: {""}
           <input
+            type="password"
             value={password}
             onChange={(event) => setPassword(event.target.value)}
           />
